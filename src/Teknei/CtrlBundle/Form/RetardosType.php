4ceaa2514ea7e502411482d536f983873a9762f4
mnieto <mnieto@mnieto-PC>
2016-05-10 20:09:37 -0500

    Eliminacion de tipo de dato 'time'

--- src/Teknei/CtrlBundle/Form/RetardosType.php
+++ src/Teknei/CtrlBundle/Form/RetardosType.php
@@ -15,7 +15,7 @@ class RetardosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('horaLlega', 'time')
+            ->add('horaLlega')
             ->add('idrecaSema')
             ->add('idesta')
             ->add('idusuario')
